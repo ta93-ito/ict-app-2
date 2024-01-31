a4c7e6cc8bad342a055f29a46477c1833414bede
ta93-ito <tkm.ito@outlook.jp>
2024-01-31 20:04:07 +0900

    dentaku lv.1

--- dentaku/main.py
+++ dentaku/main.py
@@ -1,25 +1,15 @@
-# 電卓 Level0
-
-import sys
 import tkinter as tk
 
-# メイン関数
-def main():
-    root = tk.Tk()
-    root.title("電卓L0")
-    den = Dentaku(root)
-    root.mainloop()
-
-# 電卓クラス
 class Dentaku():
-    # 作成
     def __init__(self, root):
-        self.tf = tk.Frame(root)    # トップレベルのフレーム
+        self.tf = tk.Frame(root)
         self.tf.grid(column = 0, row = 0, padx = 15, pady = 15)
 
-        # ボタンを配置
+        self.current = "0"
+        self.operation = None
+        self.result = None
+
         ButtonDef = (
-        #     行 列 ラベル 関数
             (4, 0, "0", self.numinput),
             (3, 0, "1", self.numinput),
             (3, 1, "2", self.numinput),
@@ -30,46 +20,67 @@ class Dentaku():
             (1, 0, "7", self.numinput),
             (1, 1, "8", self.numinput),
             (1, 2, "9", self.numinput),
-            (4, 1, "*", self.mul),
-            (4, 2, "/", self.div),
-            (1, 3, "-", self.sub),
-            (2, 3, "+", self.add),
-            (3, 3, "=", self.equal),
+            (4, 1, "*", self.set_operation),
+            (4, 2, "/", self.set_operation),
+            (1, 3, "-", self.set_operation),
+            (2, 3, "+", self.set_operation),
+            (3, 3, "=", self.calculate),
             (4, 3, "C", self.clear))
 
         root.option_add('*Button.font', 'ＭＳゴシック 28')
         for r, c, label, func in ButtonDef:
-            Button = tk.Button(self.tf, text = label)
+            Button = tk.Button(self.tf, text=label)
             Button.bind("<Button-1>", func)
-            Button.grid(column = c, row = r, sticky = tk.N +tk.E + tk.S + tk.W)
+            Button.grid(column=c, row=r, sticky=tk.N + tk.E + tk.S + tk.W)
 
-        # 数字が表示される「エントリー」
         root.option_add('*Entry.font', 'ＭＳゴシック 32')
-        self.NumBox = tk.Entry(self.tf, width = 10, justify = tk.RIGHT)
+        self.NumBox = tk.Entry(self.tf, width=10, justify=tk.RIGHT)
         self.NumBox.insert(tk.END, "0")
-        self.NumBox.grid(column = 0, columnspan = 4, row = 0)
+        self.NumBox.grid(column=0, columnspan=4, row=0)
 
-    # ボタン毎の動作を定義（イベントドライバ群）
-    def numinput(self, e):            # 数字キー
-        pass
+    def numinput(self, e):
+        num = e.widget.cget("text")
+        if self.current == "0":
+            self.current = num
+        else:
+            self.current += num
+        self.update_numbox()
 
-    def mul(self, e):                # ×
-        pass
+    def set_operation(self, e):
+        if not self.result:
+            self.result = int(self.current)
+        self.current = "0"
+        self.operation = e.widget.cget("text")
 
-    def div(self, e):                # ／
-        pass
+    def calculate(self, e):
+        if self.operation:
+            if self.operation == "+":
+                self.result += int(self.current)
+            elif self.operation == "-":
+                self.result -= int(self.current)
+            elif self.operation == "*":
+                self.result *= int(self.current)
+            elif self.operation == "/":
+                self.result /= int(self.current)
+            self.current = str(self.result)
+            self.update_numbox()
+            self.operation = None
 
-    def sub(self, e):                # －
-        pass
+    def clear(self, e):
+        self.current = "0"
+        self.operation = None
+        self.result = None
+        self.update_numbox()
 
-    def add(self, e):                # ＋
-        pass
+    def update_numbox(self):
+        self.NumBox.delete(0, tk.END)
+        self.NumBox.insert(tk.END, self.current)
 
-    def equal(self, e):                # ＝
-        pass
-
-    def clear(self, e):                # Ｃ
-        pass
+def main():
+    root = tk.Tk()
+    root.title("電卓L1")
+    den = Dentaku(root)
+    root.mainloop()
 
 if __name__ == '__main__':
     main()

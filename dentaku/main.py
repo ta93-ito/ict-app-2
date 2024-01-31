b9507877d095124f6a65887f0f3de66654d81e37
ta93-ito <tkm.ito@outlook.jp>
2024-01-31 20:22:06 +0900

    dentaku lv.2

--- dentaku/main.py
+++ dentaku/main.py
@@ -3,11 +3,12 @@ import tkinter as tk
 class Dentaku():
     def __init__(self, root):
         self.tf = tk.Frame(root)
-        self.tf.grid(column = 0, row = 0, padx = 15, pady = 15)
+        self.tf.grid(column=0, row=0, padx=15, pady=15)
 
         self.current = "0"
         self.operation = None
         self.result = None
+        self.is_decimal_mode = False
 
         ButtonDef = (
             (4, 0, "0", self.numinput),
@@ -20,12 +21,14 @@ class Dentaku():
             (1, 0, "7", self.numinput),
             (1, 1, "8", self.numinput),
             (1, 2, "9", self.numinput),
-            (4, 1, "*", self.set_operation),
-            (4, 2, "/", self.set_operation),
-            (1, 3, "-", self.set_operation),
-            (2, 3, "+", self.set_operation),
-            (3, 3, "=", self.calculate),
-            (4, 3, "C", self.clear))
+            (1, 3, "*", self.set_operation),
+            (2, 3, "/", self.set_operation),
+            (3, 3, "-", self.set_operation),
+            (4, 3, "+", self.set_operation),
+            (4, 1, ".", self.numinput),
+            (3, 4, "=", self.calculate),
+            (4, 4, "C", self.clear)
+        )
 
         root.option_add('*Button.font', 'ＭＳゴシック 28')
         for r, c, label, func in ButtonDef:
@@ -33,43 +36,49 @@ class Dentaku():
             Button.bind("<Button-1>", func)
             Button.grid(column=c, row=r, sticky=tk.N + tk.E + tk.S + tk.W)
 
-        root.option_add('*Entry.font', 'ＭＳゴシック 32')
         self.NumBox = tk.Entry(self.tf, width=10, justify=tk.RIGHT)
         self.NumBox.insert(tk.END, "0")
-        self.NumBox.grid(column=0, columnspan=4, row=0)
+        self.NumBox.grid(column=0, columnspan=5, row=0)
 
     def numinput(self, e):
         num = e.widget.cget("text")
-        if self.current == "0":
+        if self.current == "0" and num != ".":
             self.current = num
-        else:
+        elif num == "." and "." not in self.current:
+            self.current += "."
+            self.is_decimal_mode = True
+        elif num != ".":
             self.current += num
         self.update_numbox()
 
     def set_operation(self, e):
-        if not self.result:
-            self.result = int(self.current)
+        if self.result is None:
+            self.result = float(self.current)
+        else:
+            if self.operation:
+                self.calculate(None)
         self.current = "0"
         self.operation = e.widget.cget("text")
+        self.is_decimal_mode = False
 
     def calculate(self, e):
         if self.operation:
-            if self.operation == "+":
-                self.result += int(self.current)
-            elif self.operation == "-":
-                self.result -= int(self.current)
-            elif self.operation == "*":
-                self.result *= int(self.current)
-            elif self.operation == "/":
-                self.result /= int(self.current)
+            try:
+                self.result = eval(f"{self.result}{self.operation}float('{self.current}')")
+            except ZeroDivisionError:
+                self.result = "Error"
+            else:
+                self.result = int(self.result) if self.result.is_integer() else self.result
             self.current = str(self.result)
             self.update_numbox()
             self.operation = None
+            self.is_decimal_mode = False
 
     def clear(self, e):
         self.current = "0"
         self.operation = None
         self.result = None
+        self.is_decimal_mode = False
         self.update_numbox()
 
     def update_numbox(self):
@@ -78,7 +87,7 @@ class Dentaku():
 
 def main():
     root = tk.Tk()
-    root.title("電卓L1")
+    root.title("電卓")
     den = Dentaku(root)
     root.mainloop()
 
